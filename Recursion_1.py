f5c14b1ab19a1d8748538e1c0a59350f2053c034
Fiifi Dawson <edem.fiifi.dawson@gmail.com>
2023-08-18 20:04:21 +0000

    Update Recursion_1.py

--- Recursion_1.py
+++ Recursion_1.py
@@ -1,5 +1,7 @@
 from typing import List
 
+""""""
+
 def add_nums(nums: List[int]) -> int:
 
     #Base Case: nums = [3]
@@ -13,5 +15,44 @@ def add_nums(nums: List[int]) -> int:
     return first + second
 
 
-add_nums([31, 45, 123, 312])
+# add_nums([31, 45, 123, 312])
+
+
+
+"""Reverse an array"""
+def reverse(nums: List[int]) -> int:
+
+    #Base Case: [4] => [4]
+    if len(nums)==1:
+        return nums
     
+    #Recursive: [1] + [1, 2] || [2] + [1] => [2, 1]
+    first = reverse([nums[0]])
+    second = reverse(nums[1:])
+
+    return second + first
+
+
+# reverse([1, 2, 6, 7, 8 ])
+
+
+
+
+"""Factorial of a number"""
+
+def factorial(n):
+
+    # Base Case
+    if n == 1 or n == 0:
+        return 1
+    
+    # Recursive 
+    first = n
+    second = factorial(n-1)
+
+    return first * second
+
+
+
+n = 1
+print(factorial(n))
